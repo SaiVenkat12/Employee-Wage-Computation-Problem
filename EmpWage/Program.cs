d119efbbd4cfc2e0d58810c946dd8e39e334a61e
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-03 17:44:54 +0530

    [SaiVenkat]-Emplyoee Monthly Wage

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -3,34 +3,40 @@ namespace EmpWage
 {
    internal class Program
     {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int Rate_Per_Hour = 20;
+        public const int Working_Days = 20;
         static void Main(string[] args)
-
         {
-           const int IS_FULL_TIME = 2;
-           const int IS_PART_TIME = 1;
-
-            int Rate_Per_Hour = 20;
             int Hrs = 0;
             int wage = 0;
+            int TotalWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+           for (int day = 0; day<Working_Days; day++)
             {
-                case IS_PART_TIME:
-                    Hrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    Hrs = 8;
-                    break;
-                default:
-                    Hrs = 0;
-                    break;
-            }
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
 
-            wage = Hrs * Rate_Per_Hour;
-            Console.WriteLine("Employee Wage is " + wage);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        Hrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        Hrs = 8;
+                        break;
+                    default:
+                        Hrs = 0;
+                        break;
+                }
+
+                wage = Hrs * Rate_Per_Hour;
+                TotalWage+= wage;
+                Console.WriteLine("Employee Wage is " + wage);
+            }
+            
+            Console.WriteLine("Total Employee Wage is " + TotalWage);
         }
     }
 }
