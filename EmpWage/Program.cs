54562706616f63d6ea823f3c3ce90acf0979282b
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-03 17:01:54 +0530

    [SaiVenkat]-Checking Daily Wage

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -6,18 +6,24 @@ namespace EmpWage
         static void Main(string[] args)
 
         {
-            int IS_FULL_TIME = 1; 
+            int IS_FULL_TIME = 1;
+            int Rate_Per_Hour = 20;
+            int Hrs = 0;
+            int wage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Hrs=8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                Hrs=0;
             }
+            wage = Hrs * Rate_Per_Hour;
+            Console.WriteLine("Employee Wage per Day is " + wage);
         }
     }
 }
