d4ff067f82d371479d3a2a851f5876e2363d2447
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-03 17:11:47 +0530

    [SaiVenkat]-Check Emp is Part Time or not

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -6,24 +6,29 @@ namespace EmpWage
         static void Main(string[] args)
 
         {
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int Is_PART_TIME = 1;
             int Rate_Per_Hour = 20;
             int Hrs = 0;
             int wage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Hrs=8;
+                Hrs = 8;
+            }
+            else if (empCheck == Is_PART_TIME) 
+            {
+                Hrs = 4;
             }
             else
             {
-                Hrs=0;
+                Hrs = 0;
             }
             wage = Hrs * Rate_Per_Hour;
-            Console.WriteLine("Employee Wage per Day is " + wage);
+            Console.WriteLine("Employee Wage is " + wage);
         }
     }
 }
