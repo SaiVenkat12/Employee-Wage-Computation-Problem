9dc43e499444fc524b79a6d5adc7d7c0b17037f3
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-11 22:55:42 +0530

    [SaiVenkat]-Saving Employee for Each Company Seperately

---
+++ EmpWage/EmpWage/EmpWageBuilderObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    public class EmpWageBuilderObject
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string comp;
+        private int empRH;
+        private int WDays;
+        private int MHrs;
+        public int tew;
+
+        public EmpWageBuilderObject(string company, int empRatePerHrs, int numOfWorkingDays, int maxHrsPerMonth)
+        {
+            comp = company;
+            empRH = empRatePerHrs;
+            WDays = numOfWorkingDays;
+            MHrs = maxHrsPerMonth;
+        }
+        public void computeEmpWage()
+        {
+
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs < MHrs && totalWorkingDays < WDays)
+            {
+
+                Random rand = new Random();
+                int empCheck = rand.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                totalWorkingDays ++;
+
+            }
+            int totalEmpWage = totalEmpHrs * empRH;
+            Console.WriteLine("No.of Hours = " + totalEmpHrs + " and" + " No.of Working Days = " + WDays);
+            Console.Write("Total Employee Wage of the company " + comp + "  is " + totalEmpWage);
+
+        }
+    }
+}
---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 20, 80);
+
+            EmpWageBuilderObject Relaince = new EmpWageBuilderObject("Relaince", 18, 24, 100);
+
+            dMart.computeEmpWage();
+            Console.WriteLine("");
+            Relaince.computeEmpWage();
+
+        }
+
+    }
+}
